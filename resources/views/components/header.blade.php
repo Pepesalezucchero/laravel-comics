033824b0b868f3a9d16bba9c8043099e11d3bc25
Alessio Rugi <alessiorugi95@gmail.com>
2024-02-09 17:52:42 +0100

    header and mian set, web.php updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,43 @@
 <header>
-    <h1>Hello from Header</h1>
+<nav class="my_container_general my_nav_container">
+        <div id="logo">
+            <a href="#"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo pagina DC universe"></a>
+        </div>
+        <div class="right-navbar">
+            <ul>
+                <li>
+                    <a :href="#">characters</a>
+                </li>
+                <li>
+                    <a :href="#" class="selected">comics</a>
+                </li>
+                <li>
+                    <a :href="#">movies</a>
+                </li>
+                <li>
+                    <a :href="#">tv</a>
+                </li>
+                <li>
+                    <a :href="#">games</a>
+                </li>
+                <li>
+                    <a :href="#">collectibles</a>
+                </li>
+                <li>
+                    <a :href="#">videos</a>
+                </li>
+                <li>
+                    <a :href="#">fans</a>
+                </li>
+                <li>
+                    <a :href="#">news</a>
+                </li>
+                <li>
+                    <a :href="#">shop</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="nav-jumbotron"></div>
 </header>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+    <title>Laravel comics</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+</main>
+    <section id="main_backgound">
+        <div class="my_container_general my_main_container">
+            <h1 class="main-title-page">CURRENT SERIES</h1>
+
+            <ul class="comics_list">
+                @foreach ($comics as $comic)
+                    <li class="comic">
+                        <img class="comic_img" src='{{ $comic['thumb'] }}' alt='{{ $comic['title'] }}'>
+                        <span>{{ $comic['title'] }}</span>
+                    </li>
+                @endforeach
+            </ul>
+
+            <div class="btn">
+                <button>LOAD MORE</button>
+            </div>
+        </div>
+    </section>
+<main>
+
 @endsection
