87448b12e1220037237e84857299eb4b4dd3f008
Alessio Rugi <alessiorugi95@gmail.com>
2024-02-09 18:17:19 +0100

    footer set

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,51 @@
 <footer>
-    <h1>Hello from footer</h1>
+    <section id="footer-backgound">
+        <div class="my_container_general my_footer_container">
+            <div class="footer-left">
+                <div class="col">
+                    <ul><h2>DC COMICS</h2>
+                        <a href="#"><li>Characters</li></a>
+                        <a href="#"><li>Comics</li></a>
+                        <a href="#"><li>Movies</li></a>
+                        <a href="#"><li>TV</li></a>
+                        <a href="#"><li>Games</li></a>
+                        <a href="#"><li>Videos</li></a>
+                        <a href="#"><li>News</li></a>
+                    </ul>
+                    <ul><h2>SHOP</h2>
+                        <a href="#"><li>Shop DC</li></a>
+                        <a href="#"><li>Shop DC Collectibles</li></a>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul><h2>DC</h2>
+                        <a href="#"><li>Term Of Use</li></a>
+                        <a href="#"><li>Privacy policy (New)</li></a>
+                        <a href="#"><li>Ad Choices</li></a>
+                        <a href="#"><li>Advertising</li></a>
+                        <a href="#"><li>Jobs</li></a>
+                        <a href="#"><li>Subscriptions</li></a>
+                        <a href="#"><li>Talent Workshops</li></a>
+                        <a href="#"><li>CPSC Certificates</li></a>
+                        <a href="#"><li>Ratings</li></a>
+                        <a href="#"><li>Shop Help</li></a>
+                        <a href="#"><li>Contact Us</li></a>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul><h2>SITES</h2>
+                        <a href="#"><li>DC</li></a>
+                        <a href="#"><li>MAD Magazine</li></a>
+                        <a href="#"><li>DC Kids</li></a>
+                        <a href="#"><li>DC Universe</li></a>
+                        <a href="#"><li>DC Power Visa</li></a>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-right">
+                <div class="logo-jumbotron"></div>
+            </div>
+
+        </div>
+    </section>
 </footer>
